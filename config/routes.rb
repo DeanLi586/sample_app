924f19776d280705b0bf0a98c085554bbeea87c5
Dean Li <danagbemava@gmail.com>
2017-07-19 17:21:47 +0000

    Implement basic login

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,16 @@
 Rails.application.routes.draw do
+  # get 'sessions/new'
   get 'users/new'
-
   get 'static_pages/home'
-
   get '/help' , to: 'static_pages#help'
-
   get '/about', to:'static_pages#about'
-
   get '/contact', to:'static_pages#contact'
-
   get '/signup', to: 'users#new'
-
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
   post '/signup', to: 'users#create'
+  delete '/logout', to: 'sessions#destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#home'
 
