b7d2f70eb2cd459992121c07b546e8f7450cd862
Dean Li <danagbemava@gmail.com>
2017-07-22 16:53:10 +0000

    added password reset

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
 
-    attr_accessor :remember_token, :activation_token
+    attr_accessor :remember_token, :activation_token, :reset_token
     before_save :downcase_email
     before_create :create_activation_digest
     validates :name, presence: true, length: { maximum: 50 }
@@ -26,25 +26,46 @@ class User < ApplicationRecord
     update_attribute(:remember_digest, User.digest(remember_token))
   end
 
+  # activates account
   def activate
     update_attribute(:activated, true)
     update_attribute(:activated_at, Time.zone.now)
   end
 
+  # sends account activation email
   def send_activation_email
     UserMailer.account_activation(self).deliver_now
   end
 
+  # set the password reset attributes
+  def create_reset_digest
+    self.reset_token = User.new_token
+    update_attribute(:reset_digest, User.digest(reset_token))
+    update_attribute(:reset_sent_at, Time.zone.now)
+  end
+
+  # send password reset email
+  def send_password_reset_email
+    UserMailer.password_reset(self).deliver_now
+  end
+
+  # checks if account is authenticated
   def authenticated?(attribute, token)
     digest = send("#{attribute}_digest")
     return false if digest.nil?
     BCrypt::Password.new(digest).is_password?(token)
   end
 
+  # forgets a user
   def forget  
     update_attribute(:remember_digest, nil)
   end
 
+  # returns true if the password reset has expired
+  def password_reset_expired?
+    reset_token < 2.hours.ago
+  end
+  
   private 
     def downcase_email
       self.email = email.downcase
